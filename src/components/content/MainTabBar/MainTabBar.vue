92a0d1ce9af563fbf3d075cc524b743b2d23ac31
qll <1445663561@qq.com>
2020-07-25 00:08:49 +0800

    prepare

---
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~assets/img/home-deactivated.svg">
+      <img slot="item-icon-active" src="~assets/img/home-activated.svg">
+      <template v-slot:item-text><div>首页</div></template>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/category-deactivated.svg">
+      <img slot="item-icon-active" src="~assets/img/category-activated.svg">
+      <template #item-text><div>分类</div></template>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/cart-deactivated.svg">
+      <img slot="item-icon-active" src="~assets/img/cart-activated.svg">
+      <template v-slot:item-text><div>购物车</div></template>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/my-deactivated.svg">
+      <img slot="item-icon-active" src="~assets/img/my-activated.svg">
+      <template v-slot:item-text><div>我的</div></template>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "components/common/tabbar/TabBar";
+  import TabBarItem from "components/common/tabbar/TabBarItem";
+    export default {
+        name: "MainTabBar",
+      components:{
+        TabBar,
+        TabBarItem
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+<!--    <nav-bar><div slot="center">购物车</div></nav-bar>-->
+    <nav-bar><template v-slot:center><div>购物车</div></template></nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  export default {
+    name: "Home",
+    components:{
+      NavBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
